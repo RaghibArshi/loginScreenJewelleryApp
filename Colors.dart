add2cd8e513c571e3f2501dc6662df7260ea5421
Raghib Arshi <46123959+RaghibArshi@users.noreply.github.com>
2024-01-05 12:26:15 +0530

    Create Colors.dart

---
+++ Colors.dart
@@ -0,0 +1,37 @@
+import 'package:flutter/material.dart';
+
+const Color brownDarkColor = Color(0xff6f4b3e);
+const Color brownLightColor = Color(0xffB77D68);
+const Color lightTextColor = Color(0xffAE445A);
+const Color whiteTextColor = Colors.white;
+const Color darkTextColor = Color(0xff662549);
+const Color shadowColor = Colors.black;
+const Color userTypeCardClr = Colors.white;
+const Color loginCardClr = Colors.white;
+const Color facebookBtnColor = Color(0xff0919A8);
+const Color greyColor = Color(0xffD5D5D5);
+const Color lightGreyColor = Color(0xff8E8E8E);
+const Color lightBrownColor = Color(0xffA5715E);
+const Color creamColor = Color(0xffFBF1EE);
+const Color lightBlueColor = Color(0xffF5FAFF);
+const Color lightCreamColor = Color(0xffDBC3C3);
+const Color mediumGreyColor = Color(0xffF3F3F3);
+const Color lightWhiteColor = Color(0xffF8F8F8);
+const Color lightBlackColor = Color(0xffAFAFAF);
+const Color lightPinkColor = Color(0xffFBF1EE);
+const Color fadeColor = Color(0xffACACAC);
+const Color greenColor = Color(0xff004238);
+const Color shippingTextGreyColor = Color(0xffB1B1B1);
+const Color expectedDeliveryTextColor = Color(0xff959595);
+const Color lightGreenColor = Color(0xff52CC16);
+const Color greyBorderColor = Color(0xffDBDBDB);
+const Color whiteShadeColor = Color(0xffF8F8F8);
+const Color deepBackgroundColor = Color(0xFF3B0764);
+const Color lightBackgroundColor = Color(0xFFFFD1E3);
+
+
+
+
+
+
+
